936e7025c1671ef22ba5e97f931126e79193ece0
Terror-1 <elfarhosam487@gmail.com>
2022-05-01 00:06:54 +0200

    interpreter queues

--- Operating_System/src/scheduler.java
+++ Operating_System/src/scheduler.java
@@ -1,4 +1,18 @@
+import java.util.*;
 
 public class scheduler {
+  Queue<Integer> readyQueue;
+  Queue<Integer> blockedQueue; 
+  public scheduler() {
+	  this.readyQueue = new LinkedList<>();
+	  this.blockedQueue = new LinkedList<>();
+  }
+  public void runScheduler() {
+	  System.out.println("OS starts running the Scheduler");
+  }
+  public static void main(String[] args) {
+	scheduler sc = new scheduler();
 
+    System.out.println(sc.blockedQueue.contains(5));
+}
 }
